1f11cac8282dba5cc33fc8d79c682c9abf15d327
Ayoub Eddaoudi <eddaouddi.ayoub@gmail.com>
2023-03-19 00:47:27 +0100

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+REPO_URL=https://github.com/ayoubedd/.dotfiles
+DOTFILES_DIR="$HOME/.dotfiles"
+USER=$(whoami)
+PACKAGES=(base-devel zsh rustup go stow gdb nasm ncdu lf alacritty vim git python python-pip tree tmux docker docker-compose downgrade zip unzip)
+
+if [ "$USER" == "root" ]
+then
+    echo "[WARNING] Its not recommended to run this script as root."
+    sleep 5
+fi
+
+echo [INFO] Installing system packages.
+sudo pacman --needed -Syuu ${PACKAGES[@]} <<< Y
+
+echo [INFO] Cloning .dotfiles repo.
+git clone --recurse-submodules "$REPO_URL" "$DOTFILES_DIR"
+
+if [ "$?" != "0" ]
+then
+    exit 1
+fi
+
+cd ~/.dotfiles
+
+echo [INFO] Stowing your config files.
+stow -vSt ~/test $(find . -maxdepth 1 -type d -not -path './.git' -not -path . | tr -d './')
+
+echo [INFO] Done.
