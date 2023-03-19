08a044b7b7e1e20494bf713f9e374707031ef89a
Ayoub Eddaoudi <eddaouddi.ayoub@gmail.com>
2023-03-19 10:38:16 +0000

    fix window picker issues

--- rofi/.config/rofi/modi/window_picker.sh
+++ rofi/.config/rofi/modi/window_picker.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# Run
-rofi \
-  -show window \
-	-kb-cancel Escape
+# Get available windows
+windows=$(swaymsg -t get_tree | jq -r '.nodes[1].nodes[].nodes[] | .. | (.id|tostring) + " " + .name?' | grep -e "[0-9]* ."  )
+
+# Select window with rofi
+selected=$(echo "$windows" | rofi -dmenu -i | awk '{print $1}')
+
+# Tell sway to focus said window
+swaymsg [con_id="$selected"] focus
