d8a88303e05c87ca74c78a2f13349361fa18ba91
Ayoub Eddaoudi <eddaouddi.ayoub@gmail.com>
2023-03-20 08:12:02 +0000

    make zsh default script interter & symlink prezto config

--- setup.sh
+++ setup.sh
@@ -1,26 +1,26 @@
-#!/bin/bash
+#!/bin/zsh
 
 REPO_URL=https://github.com/ayoubedd/.dotfiles
 DOTFILES_DIR="$HOME/.dotfiles"
 USER=$(whoami)
 PACKAGES=(base-devel zsh rustup go stow gdb nasm ncdu lf alacritty vim git python python-pip tree tmux docker docker-compose downgrade zip unzip sway swaybg waybar wofi light zathura swayimg)
 
-if [ "$USER" == "root" ]
+if [[ "$USER" == "root" ]]
 then
     echo "[WARNING] Its not recommended to run this script as root."
     sleep 5
 fi
 
-echo [INFO] Installing system packages.
+echo '[INFO] Installing system packages.'
 sudo pacman --needed -Syuu ${PACKAGES[@]} <<< Y
 
-echo [INFO] Cloning .dotfiles repo.
+echo '[INFO] Cloning .dotfiles repo.'
 if [ -d "$DOTFILES_DIR" ]
 then
     cd "$DOTFILES_DIR"
     if [ ! -d "$DOTFILES_DIR/.git" ]
     then
-      echo [ERROR] existing \"$DOTFILES_DIR\" is not a repositry.
+      echo "[ERROR] existing \"$DOTFILES_DIR\" is not a repositry."
       exit 1
     fi
     git pull
@@ -35,7 +35,13 @@ fi
 
 cd "$DOTFILES_DIR"
 
-echo [INFO] Stowing your config files.
-stow -vSt ~/test $(find . -maxdepth 1 -type d -not -path './.git' -not -path . | tr -d './')
+echo '[INFO] Stowing your config files.'
+stow -vSt ~/ $(find . -maxdepth 1 -type d -not -path './.git' -not -path . | tr -d './')
 
-echo [INFO] Done.
+echo '[INFO] Symlinking zsh config'
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+echo '[INFO] Done.'
