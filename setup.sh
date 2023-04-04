bda7ef2c6d9bb143006e1cc72d311b21506000d5
Ayoub Eddaoudi <eddaouddi.ayoub@gmail.com>
2023-04-04 21:20:58 +0000

    refactor setup script

--- setup.sh
+++ setup.sh
@@ -1,15 +1,42 @@
 #!/bin/zsh
 
+set -e
+
 REPO_URL=https://github.com/ayoubedd/.dotfiles
 DOTFILES_DIR="$HOME/.dotfiles"
 USER=$(whoami)
-PACKAGES=(base-devel zsh rustup go stow gdb nasm ncdu lf alacritty vim git python python-pip tree tmux docker docker-compose downgrade zip unzip sway swaybg waybar wofi light zathura swayimg paru greetd grim wtype slurp swappy kanshi curl wget axel neovim wl-clipboard mako thunar papirus-icon-theme blueman)
+
+# Some directories to created
+DIRS=(Downloads Pictures/{Screenshots,Personal} \
+  Videos/{Recordings,Movies} Documents \
+  code/{ayoub,orbit})
+
+# A list of packges to be installed
+PACKAGES=(base-devel zsh rustup go \
+  stow gdb nasm ncdu lf alacritty vim \
+  git python python-pip tree tmux docker \
+  docker-compose downgrade zip unzip sway \
+  paru greetd grim wtype slurp swappy \
+  kanshi curl wget axel wl-clipboard \
+  mako thunar papirus-icon-theme blueman \
+  swaybg waybar wofi light zathura zathura-pdf-mupdf \
+  swayimg noto-fonts noto-fonts-emoji otf-font-awesome \
+  papirus-icon-theme powertop ttf-roboto)
+
+AUR_PACKAGES=(greetd-tuigreet catppuccin-cursors-mocha catppuccin-gtk-theme-mocha neovim-git)
 
 if [[ "$USER" == "root" ]]
 then
     echo "[WARNING] Its not recommended to run this script as root."
     sleep 5
 fi
+
+echo '[INFO] Creating home directories.'
+for dir in ${DIRS[@]}
+do
+  echo "[INFO] Creating => \"~/$dir/\""
+  mkdir -p "$HOME/$dir/"
+done
 
 echo '[INFO] Installing system packages.'
 sudo pacman --needed -Sy ${PACKAGES[@]}
@@ -28,23 +55,18 @@ else
     git clone --recurse-submodules "$REPO_URL" "$DOTFILES_DIR"
 fi
 
-if [ "$?" != "0" ]
-then
-    exit 1
-fi
-
 cd "$DOTFILES_DIR"
 
 echo '[INFO] Stowing your config files.'
 stow -vSt ~/ $(find . -maxdepth 1 -type d -not -path './.git' -not -path . | tr -d './')
 
-echo '[INFO] Symlinking zsh config'
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
+# echo '[INFO] Symlinking zsh config'
+# setopt EXTENDED_GLOB
+# for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+#   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+# done
 
 echo '[INFO] Insalling AUR packages.'
-paru --needed -S greetd-tuigreet catppuccin-cursors-mocha catppuccin-gtk-theme-mocha
+paru --needed -S ${AUR_PACKAGES[@]}
 
 echo '[INFO] Done.'
