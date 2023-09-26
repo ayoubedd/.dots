4afb9debf8eaefd1adbda3f95e76b9f6bfe42119
Ayoub Eddaoudi <eddaouddi.ayoub@gmail.com>
2023-09-26 18:04:00 +0100

    chore(setup.sh): add lazygit to setup script

--- setup.sh
+++ setup.sh
@@ -28,7 +28,7 @@ PACKAGES=(base-devel zsh rustup go bat mpv pacman-contrib \
   swayimg noto-fonts noto-fonts-emoji otf-font-awesome \
   papirus-icon-theme ttf-roboto cliphist xdg-user-dirs \
   xdg-desktop-portal xdg-desktop-portal-wlr procps-ng \
-  nm-connection-editor fx thermald)
+  nm-connection-editor fx thermald lazygit)
 
 AUR_PACKAGES=(catppuccin-cursors-mocha wl-color-picker \
   catppuccin-gtk-theme-mocha neovim-git swaylock-effects-git \
