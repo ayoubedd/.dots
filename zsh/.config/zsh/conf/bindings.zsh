bea81419804a5953ade959650c239ecc6ac17917
Ayoub Eddaoudi <eddaouddi.ayoub@gmail.com>
2024-02-02 12:42:08 +0100

    ommit loading vim surround, since already done by zsh-vi-mode

--- zsh/.config/zsh/conf/bindings.zsh
+++ zsh/.config/zsh/conf/bindings.zsh
@@ -13,12 +13,3 @@ bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'l' vi-forward-char
 bindkey -M menuselect 'j' vi-down-line-or-history
 bindkey -M menuselect '^[[Z' reverse-menu-complete
-
-autoload -Uz surround
-zle -N delete-surround surround
-zle -N add-surround surround
-zle -N change-surround surround
-bindkey -M vicmd cs change-surround
-bindkey -M vicmd ds delete-surround
-bindkey -M vicmd ys add-surround
-bindkey -M visual S add-surround
