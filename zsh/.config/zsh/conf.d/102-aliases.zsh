c12beee7e995fa410f7729725cfda5ab9a13935a
Ayoub Eddaoudi <me@ayoubedd.me>
2024-05-05 16:03:40 +0100

    chore(zsh): update open alias to o

--- zsh/.config/zsh/conf.d/102-aliases.zsh
+++ zsh/.config/zsh/conf.d/102-aliases.zsh
@@ -25,7 +25,7 @@ alias cat="bat --plain"
 alias grep="grep --color=auto"
 alias mkdir="mkdir -p"
 
-alias open="xdg-open"
+alias o="xdg-open"
 alias path='echo -e ${PATH//:/\\n}'
 alias pg="ping 1.0.0.1 -c 5"
 alias vi=nvim
