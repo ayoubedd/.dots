c13276e39a5c1c43f15b7f90995f1a6e5a21430f
Ayoub Eddaoudi <me@ayoubedd.me>
2024-05-13 19:29:28 +0100

    chore(nvim): add jj keybind to switch back to normal mode

--- nvim/.config/nvim/lua/keybinds/insert.lua
+++ nvim/.config/nvim/lua/keybinds/insert.lua
@@ -1,3 +1,13 @@
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
+
+local function imap(key, cmd, local_opts)
+  if not local_opts then
+    map("i", key, cmd, opts) -- close current window
+  else
+    map("i", key, cmd, local_opts) -- close current window
+  end
+end
+
+imap("jj", "<esc>") -- return to normal mode
