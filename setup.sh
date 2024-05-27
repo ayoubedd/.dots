0ce943827166da1b87094f3d6cd71028e624a8d1
Ayoub Eddaoudi <me@ayoubedd.me>
2024-05-27 13:11:17 +0100

    chore(setup): add overskride to AUR packages

--- setup.sh
+++ setup.sh
@@ -74,7 +74,7 @@ PACKAGES=(base-devel zsh rustup go bat mpv pacman-contrib \
 AUR_PACKAGES=(catppuccin-cursors-mocha wl-color-picker \
   catppuccin-gtk-theme-mocha neovim-git swaylock-effects-git \
   wofi-emoji-git battop ripdrag-git pam-fprint-grosshack \
-  ldm macchina-bin)
+  ldm macchina-bin overskride)
 
 if [[ "$USER" == "root" ]]
 then
